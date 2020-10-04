a646b34ce4f7536c76fce16579f14ae0b8ead784
Eduardo Sarmento <cealvesarmento@gmail.com>
2020-10-04 15:27:20 -0300

    :sparkles: Cria Recipe Card

---
+++ Larica-Vue/src/components/recipecard/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <li>
+    <img :src="thumbnail" :alt="title" />
+    <h3>{{ title }}</h3>
+    <p>{{ ingredients }}</p>
+  </li>
+</template>
+
+<script>
+  export default {
+    name: 'recipecard',
+    props: ['recipe'],
+    data() {
+      return {
+        title: this.recipe.title,
+        ingredients: this.recipe.ingredients,
+        thumbnail: this.recipe.thumbnail
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  li {
+    cursor: pointer;
+  }
+
+  img {
+    width: 100%;
+    height: 200px;
+    border-radius: 20px;
+  }
+
+  h3 {
+    font-size: 1.3rem;
+    font-weight: 500;
+  }
+
+  p {
+    color: #b0b0b0;
+  }
+
+</style>
